0e410bff286f8297df227befa694bec45f660392
Pedro Markun <pedro@markun.com.br>
2017-08-30 12:04:42 -0300

    modelo da agenda

--- agenda/models.py
+++ agenda/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
+class StringField(models.TextField):
+    def formfield(self, **kwargs):
+        kwargs['widget'] = widgets.AdminTextInputWidget
+        return super(StringField, self).formfield(**kwargs)
+
 # Create your models here.
+class Partido(models.Model):
+    nome = StringField()
+    sigla =StringField()
+    logo = models.ImageField(blank=True, upload_to='partidos')
+
+class Vereador(models.Model):
+    nome = StringField()
+    partido = models.ForeignKey(Partido)
+
+class Local(models.Model):
+    nome = StringField()
+
+class Agenda(models.Model):
+    horario_inicio = models.DateTimeField()
+    horario_fim = models.DateTimeField()
+    descricao = models.TextField()
+    local = models.ForeignKey(Local)
+    vereador = models.ForeignKey(Vereador)
--- gabinete56/settings.py
+++ gabinete56/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'agenda.app.AgendaConfig',
+    'agenda.apps.AgendaConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
